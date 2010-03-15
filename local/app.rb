51810cbcd36de8bbf696fccabbbe477fc05fbeb0
Joe Fiorini <joe@faithfulgeek.org>
2010-03-14 21:05:29 -0400

    Display next event on the homepage

--- local/app.rb
+++ local/app.rb
@@ -1,3 +1,15 @@
 require 'lib/event'
 
 set :haml, { :format => :html5, :escape_html => true, :attr_wrapper => '"' }
+
+helpers do
+  def partial(template, params={})
+    haml template, :layout => false, :escape_html => false, :locals => params
+  end
+end
+
+get "/" do
+  set_from_config(:google_analytics_code)
+  @next_event = Event.next
+  cache haml(:index)
+end
